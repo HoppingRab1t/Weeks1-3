fe3cfa49e41cc689f385477f3cbc76ff877a0615
Daniel Yeung <yeungdan@shernet.sheridancollege.ca>
2026-01-13 11:07:52 -0500

    Added bouncing ball

--- Assets/Scripts/Week 2 coding Gym/Bouncing ball.cs
+++ Assets/Scripts/Week 2 coding Gym/Bouncing ball.cs
@@ -1,8 +1,13 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Bouncingball : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float time;
+    public AnimationCurve curve;
+    
+
     void Start()
     {
         
@@ -11,6 +16,15 @@ public class Bouncingball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = transform.position;
+        pos.y = curve.Evaluate(time);
+        time += Time.deltaTime;
+        if (time > 1)
+        {
+            time = 0;
+        }
+        transform.position = pos;
+
+
     }
 }
