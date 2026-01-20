cf4b93895e0d6196ec9a6c9df23ffe37150d94d0
Daniel Yeung <yeungdan@shernet.sheridancollege.ca>
2026-01-20 11:05:57 -0500

    Added into script how to change colour, array lists, and sprite  render stuff

--- Assets/Scripts/Sprite Changer.cs
+++ Assets/Scripts/Sprite Changer.cs
@@ -1,3 +1,5 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,7 +7,9 @@ public class NewMonoBehaviourScript : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
     public Color col;
-
+    public List<Sprite> berrel0;
+    
+    public int randomnum;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,18 +21,28 @@ public class NewMonoBehaviourScript : MonoBehaviour
     void Update()
     {
         Vector2 pos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-        
-        //if (Keyboard.current.wKey.isPressed == true)
-        //{
-        //    pickARandomColour();
-        //}
+
+        if (Keyboard.current.wKey.isPressed == true)
+        {
+            pickARandomSprite();
+        }
+
         if ( spriteRenderer.bounds.Contains(pos))
         {
             pickARandomColour();
+
         }
         else
         {
             spriteRenderer.color = Color.white;
+        }
+
+        if (Mouse.current.leftButton.wasPressedThisFrame && berrel0.Count>0)
+        {
+            berrel0.RemoveAt(0);
+        } else
+        {
+            Debug.Log("nothing to remove");
         }
     }
     void pickARandomColour()
@@ -36,4 +50,12 @@ public class NewMonoBehaviourScript : MonoBehaviour
         //spriteRenderer.color = Random.ColorHSV();
         spriteRenderer.color = col;
     }
+    void pickARandomSprite()
+    {
+        //spriteRenderer.sprite = mySprite;
+        randomnum = Random.Range(0,berrel0.Count);
+       spriteRenderer.sprite = berrel0[randomnum];
+
+    }
+    
 }
