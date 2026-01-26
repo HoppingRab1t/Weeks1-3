2f1f63d9f6608f864ba4ce38fe7a7891512c1557
CHOCOBO_DY\danie <YeungDan@Sheridancollege.ca>
2026-01-26 11:40:01 -0500

    Added code that makes the tank spawn randomly on the screen as well as learning what is quaternion identity is (which in short sets the roation to 0,0,0)

--- Assets/Scripts/Readings folder/Week 4/Tank Spawner.cs
+++ Assets/Scripts/Readings folder/Week 4/Tank Spawner.cs
@@ -1,9 +1,13 @@
+using System.Security.Principal;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class TankSpawner : MonoBehaviour
 {
     public GameObject tankPrefab;
+    public int howMnayTanks = 0;
+
+    public FirstScript tankScript;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,7 +19,18 @@ public class TankSpawner : MonoBehaviour
     {
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
-            Instantiate(tankPrefab);
+            //Instantiate(tankPrefab, transform.position, transform.rotation );
+
+            Vector2 spawnPos = Random.insideUnitCircle * 5;
+
+            //Quanternion identiy menas no rotation which sets all of the angles to 0,0,0
+            Instantiate(tankPrefab, spawnPos, Quaternion.identity);
+
+            howMnayTanks += 1;
+        }
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Instantiate(tankPrefab, transform);
         }
     }
 }
